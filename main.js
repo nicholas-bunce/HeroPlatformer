455e4f073ab2881758e3382750d25e8b3d179570
nicholas-bunce <nicholas.bunce@hotmail.com>
2016-08-22 20:45:33 +1000

    added a level
    Added level 1 to my game.

--- main.js
+++ main.js
@@ -47,6 +47,37 @@ chuckNorris.src = "hero.png";
 var player = new Player();
 var keyboard = new Keyboard();
 
+var LAYER_COUNT = 3;
+var MAP = { tw: 60, th: 15 };
+var TILE = 35;
+var TILESET_TILE = TILE * 2;
+var TILESET_PADDING = 2;
+var TILESET_SPACING = 2;
+var TILESET_COUNT_X = 14;
+var TILESET_COUNT_Y = 14;
+
+// load the image to use for the level tiles
+var tileset = document.createElement("img");
+tileset.src = "tileset.png";
+
+function drawMap() {
+    for (var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++) {
+        var idx = 0;
+        for (var y = 0; y < level1.layers[layerIdx].height; y++) {
+            for (var x = 0; x < level1.layers[layerIdx].width; x++) {
+                if (level1.layers[layerIdx].data[idx] != 0) {
+                    // the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile), so subtract one from the tileset id to get the
+                    // correct tile
+                    var tileIndex = level1.layers[layerIdx].data[idx] - 1;
+                    var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
+                    var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_X)) * (TILESET_TILE + TILESET_SPACING);
+                    context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x * TILE, (y - 1) * TILE, TILESET_TILE, TILESET_TILE);
+                }
+                idx++;
+            }
+        }
+    }
+}
 
 function run()
 {
@@ -55,9 +86,10 @@ function run()
 	
 	var deltaTime = getDeltaTime();
 	
+	drawMap();
 	player.update(deltaTime);
 	player.draw();
-	
+
 		
 	// update the frame counter 
 	fpsTime += deltaTime;
