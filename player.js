a4079b4527cd581e42f6f733f9c67ab21869c621
nicholas-bunce <nicholas.bunce@hotmail.com>
2016-08-29 20:25:55 +1000

    Added movement sprites
    We are gonna make it!

--- player.js
+++ player.js
@@ -1,27 +1,56 @@
+var LEFT = 0;
+var RIGHT = 1;
+
+var ANIM_IDLE_LEFT = 0;
+var ANIM_JUMP_LEFT = 1;
+var ANIM_WALK_LEFT = 2;
+var ANIM_IDLE_RIGHT = 3;
+var ANIM_JUMP_RIGHT = 4;
+var ANIM_WALK_RIGHT = 5;
+var ANIM_MAX = 6;
+
+
 var Player = function () {
-    this.image = document.createElement("img");
+    this.sprite = new Sprite("ChuckNorris.png");
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+        [0, 1, 2, 3, 4, 5, 6, 7]);
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+        [8, 9, 10, 11, 12]);
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+        [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]);
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+        [52, 53, 54, 55, 56, 57, 58, 59]);
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+        [60, 61, 62, 63, 64]);
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+        [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78]);
+
+    for (var i = 0; i < ANIM_MAX; i++)
+    {
+        this.sprite.setAnimationOffset(i, -55, -87);
+    }
     this.position = new Vector2();
     this.position.set(9 * TILE, 0 * TILE);
 
     this.width = 159;
     this.height = 163;
-    
-    this.offset = new Vector2();
-    this.offset.set(-55, -87);
 
     this.velocity = new Vector2();
 
     this.falling = true;
     this.jumping = false;
 
+    this.direction = RIGHT;
 
-    this.image.src = "hero.png";
+
 };
 
 
 
 Player.prototype.update = function (deltaTime) {
     
+    this.sprite.update(deltaTime);
+
     var left = false;
     var right = false;
     var jump = false;
@@ -36,6 +65,42 @@ Player.prototype.update = function (deltaTime) {
     if (keyboard.isKeyDown(keyboard.KEY_SPACE) == true) {
         jump = true;
     }
+    if (keyboard.isKeyDown(keyboard.KEY_LEFT) == true) {
+        left = true;
+        this.direction = LEFT;
+        if(this.sprite.currentAnimation != ANIM_WALK_LEFT &&
+                this.jumping == false)
+                this.sprite.setAnimation(ANIM_WALK_LEFT);
+    }
+    else if (keyboard.isKeyDown(keyboard.KEY_RIGHT) == true) {
+        right = true;
+        this.direction = RIGHT;
+        if (this.sprite.currentAnimation != ANIM_WALK_RIGHT && 
+                this.jumping == false)
+                this.sprite.setAnimation(ANIM_WALK_RIGHT);
+    }
+    else {
+        if (this.jumping == false && this.falling == false) {
+            if (this.direction == LEFT) {
+                if (this.sprite.currentAnimation != ANIM_IDLE_LEFT)
+                    this.sprite.setAnimation(ANIM_IDLE_LEFT);
+            }
+            else {
+                if (this.sprite.currentAnimation != ANIM_IDLE_RIGHT)
+                    this.sprite.setAnimation(ANIM_IDLE_RIGHT);
+            }
+      }
+}
+    if (keyboard.isKeyDown(keyboard.KEY_SPACE) == true)
+    {
+        jump = true;
+        if (left == true) {
+                this.sprite.setAnimation(ANIM_JUMP_LEFT);
+        }
+        if (right == true) {
+                this.sprite.setAnimation(ANIM_JUMP_RIGHT);
+        }
+}
 
     var wasleft = this.velocity.x < 0;
     var wasright = this.velocity.x > 0;
@@ -51,9 +116,15 @@ Player.prototype.update = function (deltaTime) {
         ddx = ddx + ACCEL; // player wants to go right
     else if (wasright)
         ddx = ddx - FRICTION; // player was going right, but not any more
-    if (jump && !this.jumping && !falling) {
-        ddy = ddy - JUMP; // apply an instantaneous (large) vertical impulse
+    if (jump && !this.jumping && !falling)
+    {
+        // apply an instantaneous (large) vertical impulse
+        ddy = ddy - JUMP; 
         this.jumping = true;
+        if(this.direction == LEFT)
+            this.sprite.setAnimation(ANIM_JUMP_LEFT)
+        else 
+            this.sprite.setAnimation(ANIM_JUMP_RIGHT)
     }
 
     // calculate the new position and velocity:
@@ -127,9 +198,7 @@ Player.prototype.update = function (deltaTime) {
 }
 
 
-Player.prototype.draw = function () {
-    context.save();
-    context.translate(this.position.x, this.position.y);
-    context.drawImage(this.image, -this.width / 2, -this.height / 2);
-    context.restore();
+Player.prototype.draw = function ()
+{
+    this.sprite.draw(context, this.position.x, this.position.y);
 }
